847bcfc92360ffc0a32feb960a8d4e98843cf366
Bruce Schubert <bruce@emxsys.com>
2017-01-27 09:10:57 -0800

    Removed deleted files.

--- worldwind-geopkg/src/main/resources/gov/nasa/worldwind/geopkg/gpkg_spatial_ref_sys.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS gpkg_spatial_ref_sys (
-  srs_name TEXT NOT NULL,
-  srs_id INTEGER NOT NULL PRIMARY KEY,
-  organization TEXT NOT NULL,
-  organization_coordsys_id INTEGER NOT NULL,
-  definition  TEXT NOT NULL,
-  description TEXT
-);
-
