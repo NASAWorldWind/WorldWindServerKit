8194b920d207a9b075351bd037d64c34537f01b4
Bruce Schubert <bruce@emxsys.com>
2017-04-04 16:11:42 -0700

    Deprecated the installation of ECW and MrSID natives - Re: issue #18 & #19

--- worldwind-geoserver-dist/src/main/resources/windows/setup.bat
+++ worldwind-geoserver-dist/src/main/resources/windows/setup.bat
@@ -2,7 +2,7 @@
 rem -----------------------------------------------------------------------------
 rem Setup the GDAL ECW and MrSID plugins for GeoServer
 rem -----------------------------------------------------------------------------
-if exist "gdal\gdal-19-1600-x64-ecw.msi" gdal\gdal-19-1600-x64-ecw.msi
-if exist "gdal\gdal-19-1600-x64-mrsid.msi" gdal\gdal-19-1600-x64-mrsid.msi
-if exist "gdal\gdal-19-1600-ecw.msi" gdal\gdal-19-1600-ecw.msi
-if exist "gdal\gdal-19-1600-mrsid.msi"  gdal\gdal-19-1600-mrsid.msi
+rem if exist "gdal\gdal-19-1600-x64-ecw.msi" gdal\gdal-19-1600-x64-ecw.msi
+rem if exist "gdal\gdal-19-1600-x64-mrsid.msi" gdal\gdal-19-1600-x64-mrsid.msi
+rem if exist "gdal\gdal-19-1600-ecw.msi" gdal\gdal-19-1600-ecw.msi
+rem if exist "gdal\gdal-19-1600-mrsid.msi"  gdal\gdal-19-1600-mrsid.msi
