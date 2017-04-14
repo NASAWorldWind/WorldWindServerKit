e83d6fc7bfab195e30012a89328be752579fbef6
Bruce Schubert <bruce@emxsys.com>
2017-04-14 11:22:03 -0700

    Modified Linux run script to call setup.

--- worldwind-geoserver-dist/src/main/resources/linux/run.sh
+++ worldwind-geoserver-dist/src/main/resources/linux/run.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-# Environment variables for GeoServer
+## Setup the prerequisites for GeoSeriver
+source ./setup.sh
+
+## Environment variables for GeoServer
 source ./env.sh
 
-# Start the Jetty servlet container and the GeoServer webapp
+## Start the Jetty servlet container and the GeoServer webapp
 cd ${GEOSERVER_HOME}/bin
 ./startup.sh
