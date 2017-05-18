39e4565d69d6ff7fbb296f1529cfe79ec97ff4cb
Bruce Schubert <bruce@emxsys.com>
2017-05-18 08:51:36 -0700

    Removed the Windows setup.bat used to install ECW and MrSID support. - Adding ECW and/or MrSID support should be invoked by the user via the platform specific installed in the gdal folder - Re: issues #19 and #32

--- worldwind-geoserver-dist/src/main/resources/windows/setup.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-rem -----------------------------------------------------------------------------
-rem Setup the GDAL ECW and MrSID plugins for GeoServer
-rem -----------------------------------------------------------------------------
-rem if exist "gdal\gdal-19-1600-x64-ecw.msi" gdal\gdal-19-1600-x64-ecw.msi
-rem if exist "gdal\gdal-19-1600-ecw.msi" gdal\gdal-19-1600-ecw.msi
-if exist "gdal\gdal-19-1600-x64-mrsid.msi" gdal\gdal-19-1600-x64-mrsid.msi
-if exist "gdal\gdal-19-1600-mrsid.msi"  gdal\gdal-19-1600-mrsid.msi
