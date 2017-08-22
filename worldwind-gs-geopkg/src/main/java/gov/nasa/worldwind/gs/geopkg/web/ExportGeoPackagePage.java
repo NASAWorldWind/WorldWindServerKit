26046e0490c0c62e6d0a3c53b384532d68683ce9
Bruce Schubert <bruce@emxsys.com>
2017-08-22 06:42:50 -0700

    Change ExportGeoPackagePage to a functional prototype. - Added layer selection from sorted layer list - Added bounding box updates - Added tileset name and file name updates - Executes request - Generates XML

--- worldwind-gs-geopkg/src/main/java/gov/nasa/worldwind/gs/geopkg/web/ExportGeoPackagePage.java
+++ worldwind-gs-geopkg/src/main/java/gov/nasa/worldwind/gs/geopkg/web/ExportGeoPackagePage.java
@@ -17,17 +17,46 @@
  */
 package gov.nasa.worldwind.gs.geopkg.web;
 
+import gov.nasa.worldwind.geopkg.wps.GeoPackageProcessRequestTransformer;
+import com.vividsolutions.jts.geom.Envelope;
+import gov.nasa.worldwind.geopkg.wps.GeoPackageProcessRequest;
+import java.io.ByteArrayOutputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Collections;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.http.HttpServletRequest;
+import javax.xml.transform.TransformerException;
 import org.apache.wicket.Page;
 import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.form.OnChangeAjaxBehavior;
 import org.apache.wicket.ajax.markup.html.form.AjaxSubmitLink;
 import org.apache.wicket.extensions.ajax.markup.html.modal.ModalWindow;
+import org.apache.wicket.markup.html.form.CheckBox;
+import org.apache.wicket.markup.html.form.ChoiceRenderer;
+import org.apache.wicket.markup.html.form.DropDownChoice;
 import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.LoadableDetachableModel;
 import org.apache.wicket.model.Model;
+import org.apache.wicket.model.PropertyModel;
+import org.geoserver.catalog.Catalog;
+
+import org.geoserver.catalog.LayerInfo;
 import org.geoserver.ows.URLMangler;
 import org.geoserver.ows.util.ResponseUtils;
 import org.geoserver.web.GeoServerSecuredPage;
+
+import com.google.common.collect.Lists;
+import java.util.Arrays;
+import java.util.Comparator;
+import javax.xml.namespace.QName;
+import org.apache.wicket.Component;
+import org.apache.wicket.markup.html.form.FormComponent;
+import org.geotools.geometry.jts.ReferencedEnvelope;
 
 /**
  *
@@ -35,49 +64,247 @@ import org.geoserver.web.GeoServerSecuredPage;
  */
 public class ExportGeoPackagePage extends GeoServerSecuredPage {
 
-    // The output response window
-    ModalWindow responseWindow;
+    // The default model object for this page
+    private GeoPackageProcessRequest processRequest;
+    private GeoPackageProcessRequest.TilesLayer tilesLayer;
+
+    FormComponent filename;
+    FormComponent name;
 
     /**
      * Default constructor
      */
     public ExportGeoPackagePage() {
+        // Create the GeoPackageProcessRequest
+        createDefaultModel();
 
         // Create the form
         Form form = new Form("form");
         add(form);
 
-        // Setup the XML popup window
-        final ModalWindow xmlWindow = new ModalWindow("xmlWindow");
+        DropDownChoice<LayerInfo> layerChoice = initLayersDropDown("layer", form);
+
+        // Setup the tileset name field so that it can be updated by the layer choice
+        name = new TextField<>("name", new PropertyModel(tilesLayer, "name"));
+        name.setRequired(true);
+        name.setOutputMarkupId(true);
+        form.add(name);
+
+        // Setup the geopackage filename field so that it can be updated by the layer choice
+        filename = new TextField<>("filename", new PropertyModel(processRequest, "name"));
+        filename.setRequired(true);
+        filename.setOutputMarkupId(true);
+        form.add(filename);
+        
+        form.add(new TextField<>("identifier", new PropertyModel<>(tilesLayer, "identifier")));
+        form.add(new TextField<>("desc", new PropertyModel<>(tilesLayer, "description")));
+        form.add(new TextField<>("styles", new PropertyModel<>(tilesLayer, "sldBody")));
+        form.add(new TextField<>("mimeType", new PropertyModel<>(tilesLayer, "format")));
+        form.add(new TextField<>("bkgdColor", new PropertyModel<>(tilesLayer, "bgColor")));
+        form.add(new CheckBox("transparent", new PropertyModel<>(tilesLayer, "transparent")));
+
+        // lat/lon bbox
+        final BBoxPanel latLonPanel = new BBoxPanel("latLonBoundingBox", tilesLayer.getBbox());
+        latLonPanel.setOutputMarkupId(true);
+        latLonPanel.setRequired(true);
+        form.add(latLonPanel);
+
+        initLatLonBoundsLink("computeLatLon", form, layerChoice, latLonPanel);
+
+        ModalWindow responseWindow = initResponseWindow("responseWindow");
+        add(responseWindow);
+
+        ModalWindow xmlWindow = initXmlWindow("xmlWindow", responseWindow);
         add(xmlWindow);
+
+        initExecuteButton("executeProcess", form, responseWindow);
+
+        initXmlButton("executeXML", form, xmlWindow);
+
+    }
+
+    private void createDefaultModel() {
+        // Create the tiles component and initialize the default values
+        tilesLayer = new GeoPackageProcessRequest.TilesLayer();
+        tilesLayer.setStyles(Arrays.asList("raster"));
+        try {
+            tilesLayer.setSrs(new URI("EPSG:4326"));
+        } catch (URISyntaxException ex) {
+            Logger.getLogger(ExportGeoPackagePage.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        tilesLayer.setName("Hey");
+        tilesLayer.setIdentifier("L01");
+        tilesLayer.setBbox(new Envelope());
+
+        // Create the process request and initialize
+        processRequest = new GeoPackageProcessRequest();
+        processRequest.setName("Ack");
+        processRequest.addLayer(tilesLayer);
+
+        setDefaultModel(new Model(processRequest));
+    }
+
+    private DropDownChoice<LayerInfo> initLayersDropDown(final String id, Form form) {
+        // Create model with a collection of layers
+        IModel<List<LayerInfo>> layerChoices = new LoadableDetachableModel<List<LayerInfo>>() {
+            private static final long serialVersionUID = 1L;
+
+            @Override
+            protected List<LayerInfo> load() {
+                Catalog catalog = getCatalog();
+                List<LayerInfo> layerList = catalog.getLayers();
+                if (layerList == null) {
+                    return Lists.newArrayList();
+                }
+//                Filter filter = Predicates.equal("layer.type", PublishedType.RASTER);
+//                Integer limit = 100;
+//                CloseableIterator<LayerInfo> iter = catalog.list(LayerInfo.class, filter, null, limit, null);
+//                List<LayerInfo> resources = Lists.newArrayList(iter);
+
+                // Sort layers by workspace and name
+                Collections.sort(layerList, new Comparator<LayerInfo>() {
+                    @Override
+                    public int compare(LayerInfo o1, LayerInfo o2) {
+                        String ws1 = o1.getResource().getStore().getWorkspace().getName();
+                        String ws2 = o2.getResource().getStore().getWorkspace().getName();
+                        int result = ws1.compareToIgnoreCase(ws2);
+                        if (result == 0) {
+                            return o1.getName().compareTo(o2.getName());
+                        }
+                        return result;
+                    }
+                });
+                return layerList;
+
+            }
+        };
+
+        // Create the DropDown with a renderer that the displays workspace and name
+        DropDownChoice<LayerInfo> layerChoice = new DropDownChoice<>(
+                id,
+                new Model<>(),
+                layerChoices,
+                new LayerChoiceRenderer());
+
+        layerChoice.setNullValid(false); // Shows "Choose One" when null
+        layerChoice.setRequired(true);
+        layerChoice.setOutputMarkupId(true);
+        form.add(layerChoice);
+
+        // Add a listener to update model elements dependent on the selected layer
+        layerChoice.add(new OnChangeAjaxBehavior() {
+            private static final long serialVersionUID = 1L;
+
+            @Override
+            protected void onUpdate(AjaxRequestTarget target) {
+                Model<LayerInfo> layerModel = (Model<LayerInfo>) layerChoice.getDefaultModel();
+                LayerInfo layer = layerModel.getObject();
+
+                TextField<String> name = (TextField<String>) form.get("name");
+                name.setModelObject(layer.getName());
+                target.add(name);
+                
+                processRequest.setName(layer.getName());
+                filename.setModel(new PropertyModel<>(processRequest, "name"));
+                target.add(filename);
+                
+                BBoxPanel bboxPanel = (BBoxPanel) form.get("latLonBoundingBox");
+                bboxPanel.setModelObject(layer.getResource().getLatLonBoundingBox());
+                target.add(bboxPanel);
+            }
+        });
+
+        return layerChoice;
+    }
+
+    private static class LayerChoiceRenderer extends ChoiceRenderer<LayerInfo> {
+
+        private static final long serialVersionUID = 1L;
+
+        @Override
+        public Object getDisplayValue(LayerInfo layer) {
+            String workspace = layer.getResource().getStore().getWorkspace().getName();
+            return workspace + ":" + layer.getName();
+        }
+
+        @Override
+        public String getIdValue(LayerInfo layer, int index) {
+            return layer.getId();
+        }
+    }
+
+    private AjaxSubmitLink initLatLonBoundsLink(final String id, final Form form,
+            DropDownChoice<LayerInfo> layers, final BBoxPanel bboxPanel) {
+
+        AjaxSubmitLink ajaxSubmitLink = new AjaxSubmitLink(id, form) {
+            private static final long serialVersionUID = 1L;
+
+            @Override
+            public void onSubmit(final AjaxRequestTarget target, Form form) {
+                // Perform manual processing otherwise the component contents won't be updated
+                form.process(null);
+                LayerInfo layer = (LayerInfo) layers.getDefaultModelObject();
+                if (layer != null) {
+                    bboxPanel.setModelObject(layer.getResource().getLatLonBoundingBox());
+                }
+                target.add(bboxPanel);
+            }
+
+//            @Override
+//            public boolean getDefaultFormProcessing() {
+//                // Disable the default processing or the link won't trigger
+//                // when any validation fails
+//                return false;
+//            }
+        };
+        form.add(ajaxSubmitLink);
+        return ajaxSubmitLink;
+    }
+
+    /**
+     * Sets up the output response popup window.
+     */
+    private ModalWindow initResponseWindow(final String id) {
+        ModalWindow window = new ModalWindow(id);
+        window.setPageCreator(new ModalWindow.PageCreator() {
+
+            @SuppressWarnings("unchecked")
+            public Page createPage() {
+                ExecuteRequest request = new ExecuteRequest();
+                HttpServletRequest http = (HttpServletRequest) ExportGeoPackagePage.this.getRequest().getContainerRequest();
+                String url = ResponseUtils.buildURL(
+                        ResponseUtils.baseURL(http),
+                        "ows",
+                        Collections.singletonMap("strict", "true"),
+                        URLMangler.URLType.SERVICE);
+                request.setRequestUrl(url);
+
+                request.setRequestBody((String) window.getDefaultModelObject());
+//                request.setUserName(builder.username);
+//                request.setPassword(builder.password);
+                return new ResponsePage(new Model(request));
+            }
+        });
+        return window;
+    }
+
+    /**
+     * Sets up the XML popup window.
+     */
+    private ModalWindow initXmlWindow(final String id, ModalWindow responseWindow) {
+        final ModalWindow xmlWindow = new ModalWindow(id);
         xmlWindow.setPageCreator(new ModalWindow.PageCreator() {
 
             public Page createPage() {
                 return new XmlPage(xmlWindow, responseWindow, getRequestXML());
             }
         });
+        return xmlWindow;
+    }
 
-        // Setup the output response window
-        responseWindow = new ModalWindow("responseWindow");
-        add(responseWindow);
-        responseWindow.setPageCreator(new ModalWindow.PageCreator() {
-
-            @SuppressWarnings("unchecked")
-            public Page createPage() {
-                ExportRequest request = new ExportRequest(null);
-                HttpServletRequest http = (HttpServletRequest) ExportGeoPackagePage.this.getRequest().getContainerRequest();
-                String url = ResponseUtils.buildURL(ResponseUtils.baseURL(http), "ows", Collections
-                        .singletonMap("strict", "true"), URLMangler.URLType.SERVICE);
-                request.setRequestUrl(url);
-                request.setRequestBody((String) responseWindow.getDefaultModelObject());
-//                request.setUserName(builder.username);
-//                request.setPassword(builder.password);
-                return new ExportRequestResponse(new Model(request));
-            }
-        });
-
+    private void initExecuteButton(String id, Form form, ModalWindow responseWindow) {
         // Setup the Execute button
-        form.add(new AjaxSubmitLink("executeProcess") {
+        form.add(new AjaxSubmitLink(id) {
 
             @SuppressWarnings("unchecked")
             @Override
@@ -89,13 +316,14 @@ public class ExportGeoPackagePage extends GeoServerSecuredPage {
             @Override
             protected void onError(AjaxRequestTarget target, Form form) {
                 super.onError(target, form);
-//                target.add(builder.getFeedbackPanel());
                 target.add(getFeedbackPanel());
             }
         });
+    }
 
+    private void initXmlButton(String id, Form form, ModalWindow xmlWindow) {
         // Setup the Show XML button
-        form.add(new AjaxSubmitLink("executeXML") {
+        form.add(new AjaxSubmitLink(id) {
 
             @Override
             protected void onSubmit(AjaxRequestTarget target, Form form) {
@@ -113,62 +341,33 @@ public class ExportGeoPackagePage extends GeoServerSecuredPage {
                 target.add(getFeedbackPanel());
             }
         });
-
     }
 
+    /**
+     * Returns the WPS request XML based on the form's contents.
+     *
+     * @return WPS request XML
+     */
     String getRequestXML() {
-        String xml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><wps:Execute version=\"1.0.0\" service=\"WPS\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns=\"http://www.opengis.net/wps/1.0.0\" xmlns:wfs=\"http://www.opengis.net/wfs\" xmlns:wps=\"http://www.opengis.net/wps/1.0.0\" xmlns:ows=\"http://www.opengis.net/ows/1.1\" xmlns:gml=\"http://www.opengis.net/gml\" xmlns:ogc=\"http://www.opengis.net/ogc\" xmlns:wcs=\"http://www.opengis.net/wcs/1.1.1\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" xsi:schemaLocation=\"http://www.opengis.net/wps/1.0.0 http://schemas.opengis.net/wps/1.0.0/wpsAll.xsd\">\n"
-                + "    <ows:Identifier>gs:GeoPackage</ows:Identifier>\n"
-                + "    <wps:DataInputs>\n"
-                + "        <wps:Input>\n"
-                + "            <ows:Identifier>contents</ows:Identifier>\n"
-                + "            <wps:Data>\n"
-                + "                <wps:ComplexData mimeType=\"text/xml; subtype=geoserver/geopackage\">\n"
-                + "                    <geopackage xmlns=\"http://www.opengis.net/gpkg\" name=\"worldtopobathy\">\n"
-                + "                        <tiles identifier=\"L01\" name=\"world-bbox\">\n"
-                + "                            <description>World terrain and bathometry</description>\n"
-                + "                            <srs>EPSG:4326</srs>\n"
-                + "                            <bbox>\n"
-                + "                                <minx>-180</minx>\n"
-                + "                                <miny>-90</miny>\n"
-                + "                                <maxx>180</maxx>\n"
-                + "                                <maxy>90</maxy>\n"
-                + "                            </bbox>\n"
-                + "                            <coverage>\n"
-                + "                                <minZoom>0</minZoom>\n"
-                + "                                <maxZoom>4</maxZoom>\n"
-                + "                            </coverage>\n"
-                + "                            <layers>test:world</layers>\n"
-                + "                            <styles>raster</styles>\n"
-                + "                        </tiles>\n"
-                + "                        <tiles identifier=\"L02\" name=\"usa-bbox\">\n"
-                + "                            <description>USA terrain and bathometry</description>\n"
-                + "                            <srs>EPSG:4326</srs>\n"
-                + "                            <bbox>\n"
-                + "                                <minx>-171.791111</minx>\n"
-                + "                                <miny>18.91619</miny>\n"
-                + "                                <maxx>-66.96466</maxx>\n"
-                + "                                <maxy>71.357764</maxy>\n"
-                + "                            </bbox>\n"
-                + "                            <coverage>\n"
-                + "                                <minZoom>0</minZoom>\n"
-                + "                                <maxZoom>4</maxZoom>\n"
-                + "                            </coverage>\n"
-                + "                            <layers>test:world</layers>\n"
-                + "                            <styles>raster</styles>\n"
-                + "                        </tiles>\n"
-                + "                    </geopackage>\n"
-                + "                </wps:ComplexData>\n"
-                + "            </wps:Data>\n"
-                + "        </wps:Input>\n"
-                + "    </wps:DataInputs>\n"
-                + "    <wps:ResponseForm>\n"
-                + "        <wps:RawDataOutput>\n"
-                + "            <ows:Identifier>geopackage</ows:Identifier>\n"
-                + "        </wps:RawDataOutput>\n"
-                + "    </wps:ResponseForm>\n"
-                + "</wps:Execute>";
-        return xml;
+        // Update the request
+        Form form = (Form) get("form");
+        LayerInfo layer = ((DropDownChoice<LayerInfo> )form.get("layer")).getModelObject();
+        Envelope bbox = ((BBoxPanel) form.get("latLonBoundingBox")).getModelObject();
+        tilesLayer.setLayers(Arrays.asList(new QName(layer.prefixedName())));
+        tilesLayer.setBbox(bbox);
+
+        // Transform the GeoPackageProcessRequest into XML
+        GeoPackageProcessRequestTransformer transformer = new GeoPackageProcessRequestTransformer();
+        transformer.setIndentation(2);
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+        try {
+            transformer.transform(processRequest, out);
+        } catch (TransformerException e) {
+            LOGGER.log(Level.SEVERE, "Error generating xml request", e);
+            error(e);
+        }
+        return out.toString();
     }
 
 }
