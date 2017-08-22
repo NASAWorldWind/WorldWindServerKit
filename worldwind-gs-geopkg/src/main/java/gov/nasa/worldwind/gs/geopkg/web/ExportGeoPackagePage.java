5808c94102700dcea5b4c2d6bf00f7c03e811409
Bruce Schubert <bruce@emxsys.com>
2017-08-22 12:14:17 -0700

    Added the CoveragePanel to the page - min/max zoom levels are updated when the layer is changed.

--- worldwind-gs-geopkg/src/main/java/gov/nasa/worldwind/gs/geopkg/web/ExportGeoPackagePage.java
+++ worldwind-gs-geopkg/src/main/java/gov/nasa/worldwind/gs/geopkg/web/ExportGeoPackagePage.java
@@ -51,12 +51,11 @@ import org.geoserver.ows.util.ResponseUtils;
 import org.geoserver.web.GeoServerSecuredPage;
 
 import com.google.common.collect.Lists;
+import gov.nasa.worldwind.geopkg.wps.GeoPackageProcessRequest.TilesLayer.TilesCoverage;
 import java.util.Arrays;
 import java.util.Comparator;
 import javax.xml.namespace.QName;
-import org.apache.wicket.Component;
 import org.apache.wicket.markup.html.form.FormComponent;
-import org.geotools.geometry.jts.ReferencedEnvelope;
 
 /**
  *
@@ -68,8 +67,11 @@ public class ExportGeoPackagePage extends GeoServerSecuredPage {
     private GeoPackageProcessRequest processRequest;
     private GeoPackageProcessRequest.TilesLayer tilesLayer;
 
-    FormComponent filename;
-    FormComponent name;
+    DropDownChoice<LayerInfo> layerChoice;
+    TextField<String> tilesetName;
+    TextField<String> filename;
+    final BBoxPanel latLonPanel;
+    final CoveragePanel coveragePanel;
 
     /**
      * Default constructor
@@ -82,20 +84,20 @@ public class ExportGeoPackagePage extends GeoServerSecuredPage {
         Form form = new Form("form");
         add(form);
 
-        DropDownChoice<LayerInfo> layerChoice = initLayersDropDown("layer", form);
+        layerChoice = initLayersDropDown("layer", form);
 
-        // Setup the tileset name field so that it can be updated by the layer choice
-        name = new TextField<>("name", new PropertyModel(tilesLayer, "name"));
-        name.setRequired(true);
-        name.setOutputMarkupId(true);
-        form.add(name);
+        // Setup the tileset tilesetName field so that it can be updated by the layer choice
+        tilesetName = new TextField<>("name", new PropertyModel(tilesLayer, "name"));
+        tilesetName.setRequired(true);
+        tilesetName.setOutputMarkupId(true);
+        form.add(tilesetName);
 
         // Setup the geopackage filename field so that it can be updated by the layer choice
         filename = new TextField<>("filename", new PropertyModel(processRequest, "name"));
         filename.setRequired(true);
         filename.setOutputMarkupId(true);
         form.add(filename);
-        
+
         form.add(new TextField<>("identifier", new PropertyModel<>(tilesLayer, "identifier")));
         form.add(new TextField<>("desc", new PropertyModel<>(tilesLayer, "description")));
         form.add(new TextField<>("styles", new PropertyModel<>(tilesLayer, "sldBody")));
@@ -104,10 +106,15 @@ public class ExportGeoPackagePage extends GeoServerSecuredPage {
         form.add(new CheckBox("transparent", new PropertyModel<>(tilesLayer, "transparent")));
 
         // lat/lon bbox
-        final BBoxPanel latLonPanel = new BBoxPanel("latLonBoundingBox", tilesLayer.getBbox());
+        latLonPanel = new BBoxPanel("latLonBoundingBox", tilesLayer.getBbox());
         latLonPanel.setOutputMarkupId(true);
         latLonPanel.setRequired(true);
         form.add(latLonPanel);
+
+        coveragePanel = new CoveragePanel("tilesCoverage", tilesLayer.getCoverage());
+        coveragePanel.setOutputMarkupId(true);
+//        coveragePanel.setRequired(true);
+        form.add(coveragePanel);
 
         initLatLonBoundsLink("computeLatLon", form, layerChoice, latLonPanel);
 
@@ -132,13 +139,11 @@ public class ExportGeoPackagePage extends GeoServerSecuredPage {
         } catch (URISyntaxException ex) {
             Logger.getLogger(ExportGeoPackagePage.class.getName()).log(Level.SEVERE, null, ex);
         }
-        tilesLayer.setName("Hey");
-        tilesLayer.setIdentifier("L01");
         tilesLayer.setBbox(new Envelope());
+        tilesLayer.setCoverage(new TilesCoverage());
 
         // Create the process request and initialize
         processRequest = new GeoPackageProcessRequest();
-        processRequest.setName("Ack");
         processRequest.addLayer(tilesLayer);
 
         setDefaultModel(new Model(processRequest));
@@ -161,7 +166,7 @@ public class ExportGeoPackagePage extends GeoServerSecuredPage {
 //                CloseableIterator<LayerInfo> iter = catalog.list(LayerInfo.class, filter, null, limit, null);
 //                List<LayerInfo> resources = Lists.newArrayList(iter);
 
-                // Sort layers by workspace and name
+                // Sort layers by workspace and tilesetName
                 Collections.sort(layerList, new Comparator<LayerInfo>() {
                     @Override
                     public int compare(LayerInfo o1, LayerInfo o2) {
@@ -179,7 +184,7 @@ public class ExportGeoPackagePage extends GeoServerSecuredPage {
             }
         };
 
-        // Create the DropDown with a renderer that the displays workspace and name
+        // Create the DropDown with a renderer that the displays workspace and tilesetName
         DropDownChoice<LayerInfo> layerChoice = new DropDownChoice<>(
                 id,
                 new Model<>(),
@@ -199,18 +204,25 @@ public class ExportGeoPackagePage extends GeoServerSecuredPage {
             protected void onUpdate(AjaxRequestTarget target) {
                 Model<LayerInfo> layerModel = (Model<LayerInfo>) layerChoice.getDefaultModel();
                 LayerInfo layer = layerModel.getObject();
+                if (layer != null) {
+                    tilesetName.setModelObject(layer.getName());
+                    target.add(tilesetName);
 
-                TextField<String> name = (TextField<String>) form.get("name");
-                name.setModelObject(layer.getName());
-                target.add(name);
-                
-                processRequest.setName(layer.getName());
-                filename.setModel(new PropertyModel<>(processRequest, "name"));
-                target.add(filename);
-                
-                BBoxPanel bboxPanel = (BBoxPanel) form.get("latLonBoundingBox");
-                bboxPanel.setModelObject(layer.getResource().getLatLonBoundingBox());
-                target.add(bboxPanel);
+//                    processRequest.setName(layer.getName());
+//                    filename.setModel(new PropertyModel<>(processRequest, "name"));
+                    filename.setModelObject(layer.getName());
+                    target.add(filename);
+
+                    latLonPanel.setModelObject(layer.getResource().getLatLonBoundingBox());
+                    target.add(latLonPanel);
+
+                    int[] minMax = coveragePanel.findMinMaxZoom(layer, layer.getResource().getLatLonBoundingBox());
+                    TilesCoverage c = new TilesCoverage();
+                    c.setMinZoom(minMax[0]);
+                    c.setMaxZoom(minMax[1]);
+                    coveragePanel.setModelObject(c);
+                    target.add(coveragePanel);
+                }
             }
         });
 
@@ -248,6 +260,16 @@ public class ExportGeoPackagePage extends GeoServerSecuredPage {
                     bboxPanel.setModelObject(layer.getResource().getLatLonBoundingBox());
                 }
                 target.add(bboxPanel);
+
+                if (layer != null) {
+                    int[] minMax = coveragePanel.findMinMaxZoom(layer, layer.getResource().getLatLonBoundingBox());
+                    TilesCoverage c = new TilesCoverage();
+                    c.setMinZoom(minMax[0]);
+                    c.setMaxZoom(minMax[1]);
+                    coveragePanel.setModelObject(c);
+                }
+                target.add(coveragePanel);
+
             }
 
 //            @Override
@@ -349,18 +371,20 @@ public class ExportGeoPackagePage extends GeoServerSecuredPage {
      * @return WPS request XML
      */
     String getRequestXML() {
-        // Update the request
-        Form form = (Form) get("form");
-        LayerInfo layer = ((DropDownChoice<LayerInfo> )form.get("layer")).getModelObject();
-        Envelope bbox = ((BBoxPanel) form.get("latLonBoundingBox")).getModelObject();
+        // Update the request from the form before the transformation
+        LayerInfo layer = layerChoice.getModelObject();
+        Envelope bbox = latLonPanel.getModelObject();
+        TilesCoverage coverage = coveragePanel.getModelObject();
         tilesLayer.setLayers(Arrays.asList(new QName(layer.prefixedName())));
         tilesLayer.setBbox(bbox);
+        tilesLayer.setCoverage(coverage);
 
-        // Transform the GeoPackageProcessRequest into XML
+        // Setup a transformer to translate the GeoPackageProcessRequest into XML
         GeoPackageProcessRequestTransformer transformer = new GeoPackageProcessRequestTransformer();
         transformer.setIndentation(2);
         ByteArrayOutputStream out = new ByteArrayOutputStream();
 
+        // Perform the transform
         try {
             transformer.transform(processRequest, out);
         } catch (TransformerException e) {
