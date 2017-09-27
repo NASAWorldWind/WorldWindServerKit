7ac531b98fe60df4d0e7c131de9cb8689b44dc24
Bruce Schubert <bruce@emxsys.com>
2017-09-27 15:08:51 +0200

    Added EPSG:4336 LevelSet to the EnhancedWmsLayer to align tiles with GeoWebCache gridset - LevelSet corresponds to GeoServer EPSG:4326 gridset with a full sphere sector - Captured WMS config.sector as bbox property as it may differ from LevelSet's sector

--- worldwind-gs-viewer/src/main/resources/explorer/js/model/globe/layers/EnhancedWmsLayer.js
+++ worldwind-gs-viewer/src/main/resources/explorer/js/model/globe/layers/EnhancedWmsLayer.js
@@ -14,35 +14,46 @@
  * @author Bruce Schubert
  */
 define(['worldwind'],
-    function (ww) {
-        "use strict";
+        function (ww) {
+            "use strict";
 
-        var EnhancedWmsLayer = function (config, timeString) {
-            WorldWind.WmsLayer.call(this, config, timeString);
-            
-            // "tiled=true" is a hint for the GeoServer WMS to use the GeoWebCache
-            this.vendorParms = '&tiled=true';
-            
-        };
-        
-        // Inherit the WmsLayer methods
-        EnhancedWmsLayer.prototype = Object.create(WorldWind.WmsLayer.prototype);
+            var EnhancedWmsLayer = function (config, timeString) {
+                WorldWind.WmsLayer.call(this, config, timeString);
 
-        /**
-         * Returns the URL string for the resource. Overrides the TiledImageLayer
-         * resourceUrlForTile method by appending the vendor paramerters to the URL.
-         * @param {ImageTile} tile The tile whose image is returned
-         * @param {String} imageFormat The mime type of the image format desired.
-         * @returns {String} The URL string, or null if the string can not be formed.
-         */
-        EnhancedWmsLayer.prototype.resourceUrlForTile = function (tile, imageFormat) {
-            var url = WorldWind.TiledImageLayer.prototype.resourceUrlForTile.call(this, tile, imageFormat);
-            if (url) {
-                url = url + this.vendorParms;
-            }
-            return url;
-        };
+                // Extract the bbox out of the WMS layer configuration
+                this.bbox = config.sector;
 
-        return EnhancedWmsLayer;
-    }
+                // Override the default WmsLayer 36x36 level set with one that
+                // matches the GeoServer EPSG:4326 Gridset
+                this.levels = new WorldWind.LevelSet(WorldWind.Sector.FULL_SPHERE,
+                        new WorldWind.Location(90, 90),
+                        22,
+                        256,
+                        256);
+
+                // "tiled=true" is a hint for the GeoServer WMS to use the GeoWebCache
+                this.vendorParms = '&tiled=true';
+
+            };
+
+            // Inherit the WmsLayer methods
+            EnhancedWmsLayer.prototype = Object.create(WorldWind.WmsLayer.prototype);
+
+            /**
+             * Returns the URL string for the resource. Overrides the TiledImageLayer
+             * resourceUrlForTile method by appending the vendor paramerters to the URL.
+             * @param {ImageTile} tile The tile whose image is returned
+             * @param {String} imageFormat The mime type of the image format desired.
+             * @returns {String} The URL string, or null if the string can not be formed.
+             */
+            EnhancedWmsLayer.prototype.resourceUrlForTile = function (tile, imageFormat) {
+                var url = WorldWind.TiledImageLayer.prototype.resourceUrlForTile.call(this, tile, imageFormat);
+                if (url) {
+                    url = url + this.vendorParms;
+                }
+                return url;
+            };
+
+            return EnhancedWmsLayer;
+        }
 );
