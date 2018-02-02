51937c6da78338ae0d9783bacc570ae80ed5c779
Bruce Schubert <bruce@emxsys.com>
2018-02-02 06:02:07 -0800

    Updated GeoPackageImageReader to support image mosaics. - Implemented setInput. - Added getZoomLevel. - Fixed getHeight. - Fixed getWidth. - Fixed getNumImages. - Fixed getImageTypes. - Implemented getTileHeight. - Implemented getTileWidth. - Implemented isImageTiled. - Implemented read. - Implemented readTile.

--- worldwind-gt-geopkg/src/main/java/gov/nasa/worldwind/geopkg/mosaic/GeoPackageImageReader.java
+++ worldwind-gt-geopkg/src/main/java/gov/nasa/worldwind/geopkg/mosaic/GeoPackageImageReader.java
@@ -5,31 +5,34 @@
  */
 package gov.nasa.worldwind.geopkg.mosaic;
 
+import gov.nasa.worldwind.geopkg.TileEntry;
+import gov.nasa.worldwind.geopkg.TileMatrix;
 import it.geosolutions.imageio.stream.input.FileImageInputStreamExtImpl;
+import java.awt.BasicStroke;
 import java.awt.Dimension;
+import java.awt.Graphics2D;
+import java.awt.Point;
+import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
 import java.awt.image.ColorModel;
-import java.awt.image.Raster;
 import java.awt.image.SampleModel;
-import java.awt.image.WritableRaster;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.zip.DataFormatException;
+import java.util.Locale;
+import java.util.logging.Logger;
+import javax.imageio.IIOException;
 import javax.imageio.ImageReadParam;
 import javax.imageio.ImageReader;
 import javax.imageio.ImageTypeSpecifier;
 import javax.imageio.metadata.IIOMetadata;
-import javax.imageio.stream.ImageInputStream;
-import javax.media.jai.ImageLayout;
 import javax.media.jai.PlanarImage;
-import org.geotools.resources.image.ImageUtilities;
+import static org.geotools.resources.image.ImageUtilities.tileImage;
+import org.geotools.util.logging.Logging;
 import org.opengis.coverage.grid.GridEnvelope;
-import org.opengis.util.ProgressListener;
 
 /**
  *
@@ -37,32 +40,14 @@ import org.opengis.util.ProgressListener;
  */
 public class GeoPackageImageReader extends ImageReader {
 
-    /**
-     * This method sets the input only if the input object is a supported input
-     * type.
-     */
-    @Override
-    public void setInput(Object source, boolean seekForwardOnly, boolean ignoreMetadata) {
-        if (source instanceof FileImageInputStreamExtImpl) {
-            source = ((FileImageInputStreamExtImpl) source).getFile();
-//            if (LOGGER.isLoggable(Level.FINE)) {
-//                LOGGER.fine("Found a valid FileImageInputStream");
-//            }
-        }
-        super.setInput(source, seekForwardOnly, ignoreMetadata);
-
-    }
+    private final static Logger LOGGER = Logging.getLogger(GeoPackageImageReader.class.getPackage().getName());
 
     /**
      * The {@linkplain GeoPackageReader} that takes care of all the input needed
      * to read raster geopackage files.
      */
     private GeoPackageReader gpkgReader = null;
-
-    /**
-     * An array of coverage names that map to the imageIndex arguments
-     */
-    private String[] coverageNames = null;
+    private int maxZoomLevel = -1;
 
     /**
      * the flag defining if there are some listeners attached to this reader.
@@ -83,27 +68,11 @@ public class GeoPackageImageReader extends ImageReader {
      * the {@link ImageTypeSpecifier} associated to this reader.
      */
     private ImageTypeSpecifier imageType;
-
+    
     /**
-     * the hashmap holding reference of all the available images.
+     * The .gpkg input file derived from the input source.
      */
-    private HashMap<Integer, BufferedImage> imagesMap = new HashMap<Integer, BufferedImage>();
-
-    private boolean useSubSamplingAsRequestedRowcols = false;
-    private boolean castDoubleToFloating = false;
-
-//    /**
-//     * A progress monitor, set to a dummy one, in the case it is not set by the
-//     * user.
-//     */
-//    private ProgressListener monitor = new DummyProgressListener();
-    public void setUseSubSamplingAsRequestedRowcols(boolean useSubSamplingAsRequestedRowcols) {
-        this.useSubSamplingAsRequestedRowcols = useSubSamplingAsRequestedRowcols;
-    }
-
-    public void setCastDoubleToFloating(boolean castDoubleToFloating) {
-        this.castDoubleToFloating = castDoubleToFloating;
-    }
+    private File file = null;
 
     /**
      * constructs an {@link ImageReader} able to read grass raster maps.
@@ -113,6 +82,36 @@ public class GeoPackageImageReader extends ImageReader {
     public GeoPackageImageReader(GeoPackageImageReaderSpi originatingProvider) {
         super(originatingProvider);
     }
+
+    /**
+     * This method sets the input only if the input object is a supported input
+     * type.
+     *
+     * @param source
+     */
+    @Override
+    public void setInput(Object source, boolean seekForwardOnly, boolean ignoreMetadata) {
+
+        // ImageMosaics pass in GeoPackages as FileImageInputStreamExtImpl
+        if (source instanceof FileImageInputStreamExtImpl) {
+            //source = ((FileImageInputStreamExtImpl) source).getFile();
+            // Base class methods expect input to be an InputStream
+            this.input = source;
+            // Extract the File object for use by the GeoPackage class
+            this.file = ((FileImageInputStreamExtImpl) source).getFile();
+            this.seekForwardOnly = false;
+            this.ignoreMetadata = true;
+            this.minIndex = 0;
+        } else {
+            // The input is a tile's image data (invoked by the GeoPackageReader)
+            super.setInput(input, seekForwardOnly, ignoreMetadata);
+        }
+    }
+//    /**
+//     * A progress monitor, set to a dummy one, in the case it is not set by the
+//     * user.
+//     */
+//    private ProgressListener monitor = new DummyProgressListener();
 
     /**
      * Opens the GeoPackage raster file and obtains the coverage names used for
@@ -127,138 +126,226 @@ public class GeoPackageImageReader extends ImageReader {
      */
     private void ensureOpen() throws IOException {
         if (gpkgReader == null) {
-            gpkgReader = new GeoPackageReader((File) input, /*hints*/ null);
-            //
-            coverageNames = gpkgReader.getGridCoverageNames();
+            gpkgReader = new GeoPackageReader(file, /*hints*/ null);
+            maxZoomLevel = gpkgReader.getTileset().getMaxZoomLevel();
         }
     }
 
-    /*
-     * Returns the height in pixels of the given image within the
-     * input source.
+    int getZoomLevel(int imageIndex) throws IOException {
+        ensureOpen();
+        return maxZoomLevel - imageIndex;
+    }
+
+    /**
+     * Returns the height in pixels of a tile level from the current coverage
+     * within the GeoPackage.
+     *
+     * @param imageIndex overview index (mapped to a tile level)
+     * @return the height in pixels
+     * @throws IOException
      */
     @Override
     public int getHeight(final int imageIndex) throws IOException {
         ensureOpen();
-        if (gpkgReader != null) {
-            // Get the envelope surrounding the tiles found in the maximum zoom level
-            GridEnvelope gridRange = gpkgReader.getOriginalGridRange(coverageNames[imageIndex]);
-            final Dimension tileSize = ImageUtilities.toTileSize(new Dimension(gridRange.getSpan(0), gridRange.getSpan(1)));
-            return tileSize.height;
-        }
-        return -1;
-    }
-
-    public int getWidth(final int imageIndex) throws IOException {
-        ensureOpen();
-        if (gpkgReader != null) {
-            // Get the envelope surrounding the tiles found in the maximum zoom level
-            GridEnvelope gridRange = gpkgReader.getOriginalGridRange(coverageNames[imageIndex]);
-            final Dimension tileSize = ImageUtilities.toTileSize(new Dimension(gridRange.getSpan(0), gridRange.getSpan(1)));
-            return tileSize.width;
-        }
-        return -1;
+        GridEnvelope gridRange = gpkgReader.getGridRange(getZoomLevel(imageIndex));
+        return gridRange.getSpan(1);
+        // The min size returned by toTileSize is 512x512
+        //final Dimension tileSize = ImageUtilities.toTileSize(new Dimension(gridRange.getSpan(0), gridRange.getSpan(1)));
+        //return tileSize.height;
     }
 
     /**
-     * Returns the number of grid coverages in the raster GeoPackage.
+     * Returns the width in pixels of a tile level from the current coverage
+     * within the GeoPackage.
+     *
+     * @param imageIndex overview index (mapped to a tile level)
+     * @return
+     * @throws IOException
+     */
+    @Override
+    public int getWidth(final int imageIndex) throws IOException {
+        ensureOpen();
+        GridEnvelope gridRange = gpkgReader.getGridRange(getZoomLevel(imageIndex));
+        return gridRange.getSpan(0);
+    }
+
+    /**
+     * Returns the number of zoom levels. in the raster GeoPackage.
      *
      * @param allowSearch ignored
-     * @return the number of grid coverages
+     * @return the number of zoom levels
      * @throws IOException
      */
     @Override
     public int getNumImages(final boolean allowSearch) throws IOException {
         ensureOpen();
-        return gpkgReader.getGridCoverageCount();
+        return gpkgReader.getNumOverviews() + 1;
     }
 
+    /**
+     * Returns an <code>Iterator</code> containing possible image types to which
+     * the given image may be decoded, in the form of
+     * <code>ImageTypeSpecifiers</code>s. At least one legal image type will be
+     * returned.
+     *
+     * @param imageIndex the image index (mapped to a zoom level)
+     * @return
+     * @throws IOException
+     */
     @Override
     public synchronized Iterator<ImageTypeSpecifier> getImageTypes(final int imageIndex) throws IOException {
         ensureOpen();
 
-        csm = gpkgReader.getImageLayout(coverageNames[imageIndex]).getSampleModel(null);
-        ccmdl = PlanarImage.createColorModel(csm);
-        final List<ImageTypeSpecifier> l = new ArrayList<>();
-
+        // TODO: Should imageTypeSpecifiers be a member?
+        final List<ImageTypeSpecifier> imageTypeSpecifiers = new ArrayList<>();
         if (imageType == null) {
+            csm = gpkgReader.getImageLayout().getSampleModel(null);
+            ccmdl = PlanarImage.createColorModel(csm);
             imageType = new ImageTypeSpecifier(ccmdl, csm);
-            l.add(imageType);
         }
-        return l.iterator();
+        imageTypeSpecifiers.add(imageType);
+        return imageTypeSpecifiers.iterator();
     }
 
+    /**
+     * Returns the height of a tile in the given image.
+     *
+     * @param imageIndex the image index (mapped to a zoom level)
+     * @return the tile height in pixels
+     * @throws java.io.IOException
+     */
     @Override
-    public IIOMetadata getStreamMetadata() throws IOException {
-        return null;
+    public int getTileHeight(int imageIndex) throws IOException {
+        ensureOpen();
+        TileEntry tileset = gpkgReader.getTileset();
+        return tileset.getTileMatricies().get(getZoomLevel(imageIndex)).getTileHeight();
     }
 
+    /**
+     * Returns the width of a tile in the given image.
+     *
+     * @param imageIndex the image index (mapped to a zoom level)
+     * @return the width of a tile in pixels
+     * @throws java.io.IOException
+     */
     @Override
-    public IIOMetadata getImageMetadata(final int imageIndex) throws IOException {
-        return null;
+    public int getTileWidth(int imageIndex) throws IOException {
+        ensureOpen();
+        TileEntry tileset = gpkgReader.getTileset();
+        return tileset.getTileMatricies().get(getZoomLevel(imageIndex)).getTileWidth();
+    }
+
+    /**
+     * Returns if the given image is tiled (true for all raster GeoPackages).
+     *
+     * @param imageIndex ignored
+     * @return true
+     * @throws IOException
+     */
+    @Override
+    public boolean isImageTiled(int imageIndex) throws IOException {
+        // TODO: test if coverage index is raster layer
+        return true;
     }
 
 //    public void setMonitor(ProgressListener monitor) {
 //        this.monitor = monitor;
 //    }
     /**
-     * Performs the read method adding the possibility to override subsampling.
      *
-     * @param imageIndex same as
-     * {@link GrassBinaryImageReader#read(int, ImageReadParam)}
-     * @param param same as
-     * {@link GrassBinaryImageReader#read(int, ImageReadParam)}
-     * @param useSubSamplingAsRequestedRowcols a flag that gives the possibility
-     * to bypass the imageio subsampling mechanism. With GRASS maps this is
-     * often more performant in some boundary situations. In the case this flag
-     * is set to true, the subsampling values will be handled as the requested
-     * columns and rows.
-     * @param castDoubleToFloating a flag that gives the possibility to force
-     * the reading of a map as a floating point map. This is necessary right now
-     * because of a imageio bug:
-     * https://jai-imageio-core.dev.java.net/issues/show_bug.cgi?id=180
-     * @param monitor
-     * @return same as {@link GrassBinaryImageReader#read(int, ImageReadParam)}
-     * @throws IOException same as
-     * {@link GrassBinaryImageReader#read(int, ImageReadParam)}
+     * @param the image index (mapped to a zoom level)
+     * @return
+     * @throws IOException
      */
-    public BufferedImage read(final int imageIndex, ImageReadParam param,
-            boolean useSubSamplingAsRequestedRowcols, boolean castDoubleToFloating,
-            ProgressListener monitor) throws IOException {
-        ensureOpen();
-        this.useSubSamplingAsRequestedRowcols = useSubSamplingAsRequestedRowcols;
-        this.castDoubleToFloating = castDoubleToFloating;
-//        this.monitor = monitor;
-
-        return read(imageIndex, param);
-    }
-
-    @Override
-    public BufferedImage read(final int imageIndex, ImageReadParam param) throws IOException {
-        ensureOpen();
-        throw new UnsupportedOperationException("read(final int imageIndex, ImageReadParam param) not implemented!");
-    }
-
     @Override
     public BufferedImage read(final int imageIndex) throws IOException {
-        ensureOpen();
         return read(imageIndex, null);
     }
 
+    /**
+     *
+     * @param imageIndex the image index (mapped to a zoom level)
+     * @param param
+     * @return
+     * @throws IOException
+     */
     @Override
-    public Raster readRaster(final int imageIndex, ImageReadParam param) throws IOException {
+    public BufferedImage read(final int imageIndex, ImageReadParam param) throws IOException {
         ensureOpen();
-        throw new UnsupportedOperationException("readRaster(final int imageIndex, ImageReadParam param) not implemented!");
+
+        int zoomLevel = getZoomLevel(imageIndex);
+        // Get the pixel width/height of the GeoPackgae image data
+        int srcWidth = getWidth(imageIndex);
+        int srcHeight = getHeight(imageIndex);
+        
+        // Compute the pixel region of the source image that should be read, taking into
+        // account any source region and subsampling offset settings in the supplied ImageReadParam.
+        Rectangle srcRegion = getSourceRegion(param, srcWidth, srcHeight);
+        
+        // Read GeoPackage tiles for the defined pixel region into an image 
+        BufferedImage srcImage = gpkgReader.readTiles(zoomLevel, srcRegion, null);
+        
+        // Get the BufferedImage to which decoded pixel data should be written. 
+        // The image is determined by inspecting the supplied ImageReadParam if it is
+        // non-null; if its getDestination method returns a non-null value, that image is
+        // simply returned. 
+        BufferedImage destImage = getDestination(param, getImageTypes(imageIndex), srcWidth, srcHeight);
+        
+        // Copy the source image into the destination, scaling/coverting as reqd.
+        Graphics2D destGraphics = destImage.createGraphics();
+        destGraphics.drawImage(srcImage,
+                0, //int dx1,
+                0, //int dy1,
+                destImage.getWidth(), //int dx2,
+                destImage.getHeight(), //int dy2,
+                0, //int sx1,
+                0, //int sy1,
+                srcImage.getWidth(), //int sx2,
+                srcImage.getHeight(), //int sy2,
+                null); //ImageObserver observer)
+
+        // DEBUGGING: Uncomment block to draw a border around the tiles
+        /*
+        {
+            float thickness = 8;
+            destGraphics.setStroke(new BasicStroke(thickness));
+            destGraphics.drawRect(0, 0, destImage.getWidth(), destImage.getHeight());
+        }
+        */
+        destGraphics.dispose();
+        
+        return destImage;
     }
 
     /**
-     * Resets this {@link GrassBinaryImageReader}.
+     * Reads the tile indicated by the <code>tileX</code> and <code>tileY</code>
+     * arguments, returning it as a <code>BufferedImage</code>. If the arguments
+     * are out of range, an <code>IllegalArgumentException</code> is thrown. If
+     * the image is not tiled, the values 0, 0 will return the entire image; any
+     * other values will cause an <code>IllegalArgumentException</code> to be
+     * thrown.
+     *
+     * @param imageIndex the image index (mapped to a zoom level)
+     * @param tileX
+     * @param tileY
+     * @return
+     * @throws IOException
+     */
+    @Override
+    public BufferedImage readTile(int imageIndex, int tileX, int tileY) throws IOException {
+        ensureOpen();
+        return this.gpkgReader.readTile(getZoomLevel(imageIndex), tileX, tileY);
+    }
+
+    /**
+     * Resets this {@link GeoPackageImageReader}.
      */
     @Override
     public void reset() {
         dispose();
         super.setInput(null, false, false);
         gpkgReader = null;
+        file = null;
         csm = null;
         imageType = null;
     }
@@ -280,4 +367,13 @@ public class GeoPackageImageReader extends ImageReader {
 //    protected synchronized boolean abortRequested() {
 //        return gpkgReader.isAborting();
 //    }
+    @Override
+    public IIOMetadata getStreamMetadata() throws IOException {
+        return null;
+    }
+
+    @Override
+    public IIOMetadata getImageMetadata(int imageIndex) throws IOException {
+        return null;
+    }
 }
